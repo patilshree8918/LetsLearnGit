73736f9c64393452190c23816a841e0656632734
shripatil <shri8918@gmail.com>
2022-07-24 08:21:21 +0530

    with minor changes

--- diff.java
+++ diff.java
@@ -2,7 +2,7 @@ public class diff{
     public static void main(String[] args){
         int a=5;
         int b=3;
-
+        System.out.println("The difference is");
         System.out.println("The difference is"+ (a-b));
     }
 }
