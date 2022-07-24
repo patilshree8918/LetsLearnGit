22938c6b0323823135ed6696a2b3190577f0f688
shripatil <shri8918@gmail.com>
2022-07-24 08:20:48 +0530

    made some changes

--- diff.java
+++ diff.java
@@ -1,3 +1,8 @@
 public class diff{
-    
+    public static void main(String[] args){
+        int a=5;
+        int b=3;
+
+        System.out.println("The difference is"+ (a-b));
+    }
 }
--- multi.java
+++ multi.java
@@ -2,7 +2,7 @@ public class Multiply{
     public static void main(String[] args){
         int a=5;
         int b=6;
-
+        System.out.println("The Product is multiplication of two");
         System.out.println("The product is"+ (a*b));
     }
 }
